ff6c4be3a890c244d4859fdb58c913677ccef0b7
Death Note LoL <41489429+deathnotelol@users.noreply.github.com>
2023-01-05 01:04:05 +0630

    Create posts.js

---
+++ server/routes/posts.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import { getPostsBySearch, getPosts, getPost, createPost, updatedPost, likePost, deletePost } from '../controllers/posts.js';
+
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost)
+
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatedPost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+
+
+export default router;
+
